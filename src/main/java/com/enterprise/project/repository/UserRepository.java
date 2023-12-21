e5d7032b1004bff043f4ba8737597c0f758d7b3d
Caroline Lima <carolinedelima2096@gmail.com>
2023-12-21 11:23:01 -0300

    Implemented User Repository

---
+++ src/main/java/com/enterprise/project/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.enterprise.project.repository;
+
+import com.enterprise.project.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
