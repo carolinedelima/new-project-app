4c0a69ede104822ae60e3c900676f4f5d942500b
Caroline Lima <carolinedelima2096@gmail.com>
2023-12-22 19:48:23 -0300

    Implemented new API to return an user by id

--- src/main/java/com/enterprise/project/controller/UserController.java
+++ src/main/java/com/enterprise/project/controller/UserController.java
@@ -5,6 +5,7 @@ import com.enterprise.project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok(userService.getUsers());
+    }
+
+    @GetMapping
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUser(id));
     }
 }
--- src/main/java/com/enterprise/project/service/UserService.java
+++ src/main/java/com/enterprise/project/service/UserService.java
@@ -3,7 +3,9 @@ package com.enterprise.project.service;
 import com.enterprise.project.model.User;
 import com.enterprise.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -14,5 +16,10 @@ public class UserService {
 
     public List<User> getUsers() {
         return (List<User>) userRepository.findAll();
+    }
+
+    public User getUser(Long id) {
+        return userRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "User [Id] " + id + " not found."));
     }
 }
