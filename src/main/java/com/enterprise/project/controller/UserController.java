ad652a2283c87c8f805d1962e39c773cbc18faeb
Caroline Lima <carolinedelima2096@gmail.com>
2023-12-22 19:38:32 -0300

    Inject User Service into User Controller

--- src/main/java/com/enterprise/project/controller/UserController.java
+++ src/main/java/com/enterprise/project/controller/UserController.java
@@ -1,15 +1,23 @@
 package com.enterprise.project.controller;
 
+import com.enterprise.project.model.User;
+import com.enterprise.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    UserService userService;
+
     @GetMapping
-    public ResponseEntity<?> getUsers() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(userService.getUsers());
     }
 }
--- src/main/java/com/enterprise/project/service/UserService.java
+++ src/main/java/com/enterprise/project/service/UserService.java
@@ -1,7 +1,18 @@
 package com.enterprise.project.service;
 
+import com.enterprise.project.model.User;
+import com.enterprise.project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> getUsers() {
+        return (List<User>) userRepository.findAll();
+    }
 }
