8fe43db7302fbe2277008505d27fc08d8b2cd41d
Caroline Lima <carolinedelima2096@gmail.com>
2023-12-23 10:13:39 -0300

    Implemented new API to create user

--- src/main/java/com/enterprise/project/controller/UserController.java
+++ src/main/java/com/enterprise/project/controller/UserController.java
@@ -3,12 +3,11 @@ package com.enterprise.project.controller;
 import com.enterprise.project.model.User;
 import com.enterprise.project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,8 +21,14 @@ public class UserController {
         return ResponseEntity.ok(userService.getUsers());
     }
 
-    @GetMapping
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id) {
         return ResponseEntity.ok(userService.getUser(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 }
--- src/main/java/com/enterprise/project/service/UserService.java
+++ src/main/java/com/enterprise/project/service/UserService.java
@@ -22,4 +22,8 @@ public class UserService {
         return userRepository.findById(id).orElseThrow(() ->
                 new ResponseStatusException(HttpStatus.NOT_FOUND, "User [Id] " + id + " not found."));
     }
+
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
 }
