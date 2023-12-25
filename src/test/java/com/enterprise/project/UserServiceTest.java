7571f2b623cc50a547a227fc654527ab451c9d7a
Caroline Lima <carolinedelima2096@gmail.com>
2023-12-25 12:34:43 -0300

    Added JUnit dependency and User Service unit test class

---
+++ src/test/java/com/enterprise/project/UserServiceTest.java
@@ -0,0 +1,8 @@
+package com.enterprise.project;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+}
