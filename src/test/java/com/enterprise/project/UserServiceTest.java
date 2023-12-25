e1b8729cb84f4a8470711d027304161524cd4990
Caroline Lima <carolinedelima2096@gmail.com>
2023-12-25 13:06:54 -0300

    Implemented get user by id unit test

--- src/test/java/com/enterprise/project/UserServiceTest.java
+++ src/test/java/com/enterprise/project/UserServiceTest.java
@@ -12,33 +12,46 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
-
     User user = new User();
-    final Long id = 1L;
+    private static final Long USER_ID = 1L;
+    private static final String USER_NAME = "Caroline";
+    private static final String USER_EMAIL = "caroline@lima.com";
+    private static final String USER_PASSWORD = "12345";
+
     @Mock
-    UserRepository userRepository;
+    private UserRepository userRepository;
 
     @InjectMocks
-    UserService userService;
+    private UserService userService;
 
     @Before
     public void setUp() {
-        user.setId(id);
-        user.setName("Caroline");
-        user.setEmail("caroline@lima.com");
-        user.setPassword("12345");
+        user.setId(USER_ID);
+        user.setName(USER_NAME);
+        user.setEmail(USER_EMAIL);
+        user.setPassword(USER_PASSWORD);
     }
 
     @Test
     public void getUsers() {
         when(userRepository.findAll()).thenReturn(List.of(user));
-        List<User> questions = userService.getUsers();
-        Assertions.assertEquals(questions.size(), 1);
-        Assertions.assertEquals(questions.get(0), user);
+        List<User> users = userService.getUsers();
+        Assertions.assertEquals(users.size(), 1);
+        Assertions.assertEquals(users.get(0), this.user);
+    }
+
+    @Test
+    public void getUserById() {
+        when(userRepository.findById(USER_ID)).thenReturn(Optional.ofNullable(user));
+        User userById = userService.getUser(Map.of("id", USER_ID.toString()));
+        Assertions.assertNotNull(userById);
+        Assertions.assertEquals(userById, user);
     }
 }
