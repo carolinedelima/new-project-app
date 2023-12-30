ecbfc415289e81edf7c780012e5af5b769d09e89
Caroline Lima <carolinedelima2096@gmail.com>
2023-12-30 10:05:36 -0300

    Implemented get user groups

--- src/main/java/com/enterprise/project/service/UserGroupService.java
+++ src/main/java/com/enterprise/project/service/UserGroupService.java
@@ -1,7 +1,19 @@
 package com.enterprise.project.service;
 
+import com.enterprise.project.model.UserGroup;
+import com.enterprise.project.repository.UserGroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserGroupService {
+
+    @Autowired
+    UserGroupRepository userGroupRepository;
+
+    public List<UserGroup> getUserGroups() {
+        return (List<UserGroup>) userGroupRepository.findAll();
+    }
 }
