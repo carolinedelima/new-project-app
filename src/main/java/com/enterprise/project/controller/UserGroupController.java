b3277fae32fcbfb5b9c50fcee77d1f0c23686511
Caroline Lima <carolinedelima2096@gmail.com>
2023-12-30 10:06:57 -0300

    Inject User Group Service dependency into User Group Controller

--- src/main/java/com/enterprise/project/controller/UserGroupController.java
+++ src/main/java/com/enterprise/project/controller/UserGroupController.java
@@ -1,5 +1,7 @@
 package com.enterprise.project.controller;
 
+import com.enterprise.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class UserGroupController {
 
+    @Autowired
+    UserService userService;
+
     @GetMapping("/userGroups")
     public ResponseEntity<?> getUserGroups() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(userService.getUsers());
     }
 }
