b313072011766d9c066b71465d09713423860532
Caroline Lima <carolinedelima2096@gmail.com>
2023-12-30 10:01:58 -0300

    Add new endpoint to get user groups

--- src/main/java/com/enterprise/project/controller/UserGroupController.java
+++ src/main/java/com/enterprise/project/controller/UserGroupController.java
@@ -1,7 +1,16 @@
 package com.enterprise.project.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserGroupController {
+
+    @GetMapping("/userGroups")
+    public ResponseEntity<?> getUserGroups() {
+        return ResponseEntity.ok().build();
+    }
 }
