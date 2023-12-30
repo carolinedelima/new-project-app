b7ff12a59ceb8653e0c82e167de847bdabd9c6b1
Caroline Lima <carolinedelima2096@gmail.com>
2023-12-30 10:08:38 -0300

    Fix Dependency injection into User Group Controller

--- src/main/java/com/enterprise/project/controller/UserGroupController.java
+++ src/main/java/com/enterprise/project/controller/UserGroupController.java
@@ -1,21 +1,24 @@
 package com.enterprise.project.controller;
 
-import com.enterprise.project.service.UserService;
+import com.enterprise.project.model.UserGroup;
+import com.enterprise.project.service.UserGroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class UserGroupController {
 
     @Autowired
-    UserService userService;
+    UserGroupService userGroupService;
 
     @GetMapping("/userGroups")
-    public ResponseEntity<?> getUserGroups() {
-        return ResponseEntity.ok(userService.getUsers());
+    public ResponseEntity<List<UserGroup>> getUserGroups() {
+        return ResponseEntity.ok(userGroupService.getUserGroups());
     }
 }
