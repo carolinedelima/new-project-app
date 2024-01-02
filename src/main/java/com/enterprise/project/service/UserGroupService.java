64dc57c4a44c0d51550ccf9c9fefc26009fa6607
Caroline Lima <carolinedelima2096@gmail.com>
2024-01-02 10:21:52 -0300

    Improve user group service find by id

--- src/main/java/com/enterprise/project/service/UserGroupService.java
+++ src/main/java/com/enterprise/project/service/UserGroupService.java
@@ -9,7 +9,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserGroupService {
@@ -24,12 +24,7 @@ public class UserGroupService {
     public UserGroup getUserGroup(Map<String, String> params) {
         final String id = params.get("id");
         if (id != null) {
-            Optional<UserGroup> userGroup = userGroupRepository.findById(Long.valueOf(id));
-            if (userGroup.isPresent()) {
-                return userGroup.get();
-            } else {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User Group [id]" + id + " not found.");
-            }
+            return findById(Long.valueOf(id));
         } else {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Only allowed [id] param.");
         }
@@ -40,30 +35,32 @@ public class UserGroupService {
     }
 
     public void deleteUserGroup(Long id) {
-        if (userGroupRepository.findById(id).isPresent()) {
-            userGroupRepository.deleteById(id);
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User Group [id]" + id + " not found.");
-        }
+        findById(id);
+        userGroupRepository.deleteById(id);
     }
 
     public void updateUserGroup(Long id, UserGroup newUserGroup) {
-        Optional<UserGroup> oldUserGroup = userGroupRepository.findById(id);
+        final UserGroup oldUserGroup = findById(id);
+        newUserGroup.setId(id);
 
-        if (oldUserGroup.isPresent()) {
-            newUserGroup.setId(id);
-            if (newUserGroup.getGroupName() == null) {
-                newUserGroup.setGroupName(oldUserGroup.get().getGroupName());
-            }
-            if (newUserGroup.getAdminUser() == null) {
-                newUserGroup.setAdminUser(oldUserGroup.get().getAdminUser());
-            }
-            if (newUserGroup.getUserIds() == null) {
-                newUserGroup.setUserIds(oldUserGroup.get().getUserIds());
-            }
-            userGroupRepository.save(newUserGroup);
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User Group [id]" + id + " not found.");
+        if (newUserGroup.getGroupName() == null) {
+            newUserGroup.setGroupName(oldUserGroup.getGroupName());
         }
+        if (newUserGroup.getAdminUser() == null) {
+            newUserGroup.setAdminUser(oldUserGroup.getAdminUser());
+        }
+        if (newUserGroup.getUserIds() != null) {
+            Set<Long> newUserIds = oldUserGroup.getUserIds();
+            newUserIds.addAll(newUserGroup.getUserIds());
+            newUserGroup.setUserIds(newUserIds);
+        } else {
+            newUserGroup.setUserIds(oldUserGroup.getUserIds());
+        }
+        userGroupRepository.save(newUserGroup);
+    }
+
+    private UserGroup findById(Long id) {
+        return userGroupRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "User Group [id]" + id + " not found."));
     }
 }
